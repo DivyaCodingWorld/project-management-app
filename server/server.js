9b4e174ff56a7c4218582697737411107d6ebb32
DivyaCodingWorld <divyaheaven511@gmail.com>
2026-05-07 00:59:40 +0530

    server file changes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,28 @@
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// connectDB();
+
+// // Routes
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/projects", require("./routes/projectRoutes"));
+// app.use("/api/tasks", require("./routes/taskRoutes"));
+
+// app.get("/", (req, res) => {
+//   res.send("🚀 API is running...");
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -5,7 +30,10 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://project-management-app-rouge-nine.vercel.app"],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
